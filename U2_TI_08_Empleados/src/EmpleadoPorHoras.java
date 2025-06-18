d1d72746c94613cadc439c75c70a151b6de0bd32
Tony <20243ds055@utez.edu.mx>
2025-06-17 20:14:56 -0600

    Moficacion hecha

--- U2_TI_08_Empleados/src/EmpleadoPorHoras.java
+++ U2_TI_08_Empleados/src/EmpleadoPorHoras.java
@@ -1,19 +1,19 @@
 public class EmpleadoPorHoras extends Empleado{
 
-    private int horas;
+    private int horasTrabajadas;
 
-    public EmpleadoPorHoras( String id, String nombre, double salarioBase, int horas){
+    public EmpleadoPorHoras( String id, String nombre, double salarioBase, int horasTrabajadas){
         super(id, nombre, salarioBase);
-        this.horas = horas;
+        this.horasTrabajadas = horasTrabajadas;
     }
 
     @Override
     public double calcularSalario() {
         System.out.println("Nombre: " + getNombre());
-        System.out.println("Horas trabajadas: " + horas);
+        System.out.println("Horas trabajadas: " + horasTrabajadas);
         System.out.println("Salario base por hora: " + getSalarioBase());
-        System.out.println("Salario:" + (getSalarioBase() * horas));
-        return (getSalarioBase() * horas);
+        System.out.println("Salario:" + (getSalarioBase() * horasTrabajadas));
+        return (getSalarioBase() * horasTrabajadas);
     }
 
 }
